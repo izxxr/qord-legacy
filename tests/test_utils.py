cfb8a844f9c90c9ba9499eb7ed46936fef8ca4a9
NerdGuyAhmad <thisisizharahmad@gmail.com>
2022-03-27 15:22:17 +0500

    Pass float to create_timestamp in utils test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,10 +10,10 @@ class TestUtils(unittest.TestCase):
     def test_create_timestamp(self) -> None:
         # March 27 2022, 15:07:43
         time_datetime = datetime(2022, 3, 27, 15, 7, 43)
-        time_epoch = round(time_datetime.timestamp())
+        time_epoch = time_datetime.timestamp()
 
-        timestamp = f"<t:{time_epoch}>"
-        timestamp_styled = f"<t:{time_epoch}:{TimestampStyle.RELATIVE_TIME}>"
+        timestamp = f"<t:{round(time_epoch)}>"
+        timestamp_styled = f"<t:{round(time_epoch)}:{TimestampStyle.RELATIVE_TIME}>"
 
         assert utils.create_timestamp(time_datetime) == timestamp
         assert utils.create_timestamp(time_epoch) == timestamp
