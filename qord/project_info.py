7df7f846a1974487522cd74f4286795bfcfab3c7
NerdGuyAhmad <thisisizharahmad@gmail.com>
2022-04-13 19:35:57 +0500

    Bump library version to 0.3.0

--- qord/project_info.py
+++ qord/project_info.py
@@ -21,7 +21,7 @@
 # SOFTWARE.
 
 
-__version__ = "0.3.0a3"
+__version__ = "0.3.0"
 __license__ = "MIT"
 __notice__  = "Copyright (c) nerdguyahmad 2022-present. Under the MIT license."
 __author__  = "nerdguyahmad <nerdguyahmad.contact@gmail.com> <nerdguyahmad#3195>"
